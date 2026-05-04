f5d7cadfb9245bd830bee9dc9535eb8edcf95d3a
tranvanthien <thientv.24itb@vku.udn.vn>
2026-05-04 13:53:23 +0700

    add database and project zip

---
+++ quan_ly_tro.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 04, 2026 at 08:13 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quan_ly_tro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+-- Error reading structure for table quan_ly_tro.bookings: #1932 - Table &#039;quan_ly_tro.bookings&#039; doesn&#039;t exist in engine
+-- Error reading data for table quan_ly_tro.bookings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `quan_ly_tro`.`bookings`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contracts`
+--
+-- Error reading structure for table quan_ly_tro.contracts: #1932 - Table &#039;quan_ly_tro.contracts&#039; doesn&#039;t exist in engine
+-- Error reading data for table quan_ly_tro.contracts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `quan_ly_tro`.`contracts`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+-- Error reading structure for table quan_ly_tro.invoices: #1932 - Table &#039;quan_ly_tro.invoices&#039; doesn&#039;t exist in engine
+-- Error reading data for table quan_ly_tro.invoices: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `quan_ly_tro`.`invoices`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `landlord_notices`
+--
+-- Error reading structure for table quan_ly_tro.landlord_notices: #1932 - Table &#039;quan_ly_tro.landlord_notices&#039; doesn&#039;t exist in engine
+-- Error reading data for table quan_ly_tro.landlord_notices: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `quan_ly_tro`.`landlord_notices`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+-- Error reading structure for table quan_ly_tro.messages: #1932 - Table &#039;quan_ly_tro.messages&#039; doesn&#039;t exist in engine
+-- Error reading data for table quan_ly_tro.messages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `quan_ly_tro`.`messages`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+-- Error reading structure for table quan_ly_tro.notifications: #1932 - Table &#039;quan_ly_tro.notifications&#039; doesn&#039;t exist in engine
+-- Error reading data for table quan_ly_tro.notifications: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `quan_ly_tro`.`notifications`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `otp_codes`
+--
+-- Error reading structure for table quan_ly_tro.otp_codes: #1932 - Table &#039;quan_ly_tro.otp_codes&#039; doesn&#039;t exist in engine
+-- Error reading data for table quan_ly_tro.otp_codes: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `quan_ly_tro`.`otp_codes`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+-- Error reading structure for table quan_ly_tro.posts: #1932 - Table &#039;quan_ly_tro.posts&#039; doesn&#039;t exist in engine
+-- Error reading data for table quan_ly_tro.posts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `quan_ly_tro`.`posts`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table quan_ly_tro.users: #1932 - Table &#039;quan_ly_tro.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table quan_ly_tro.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `quan_ly_tro`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
